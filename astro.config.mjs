1460c7239dc107d60607351da1fa58e5c2247cb7
Eloy López <elswork@gmail.com>
2024-01-02 17:56:00 +0100

    Corregir Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,19 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
-
 import rehypeExternalLinks from 'rehype-external-links';
-
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: 
-  [
+  integrations: [
     partytown({
       // Adds dataLayer.push as a forwarding-event.
       config: {
         forward: ["dataLayer.push"],
       },
     }),
+    tailwind(), // <-- Mover tailwind() dentro del array
   ],
-  [tailwind()],
   site: 'https://david.deft.work',
   markdown: {
     rehypePlugins: [
