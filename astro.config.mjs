5a23356f0ac41901d1363d32e271bd3b239cc89d
Eloy López <elswork@gmail.com>
2024-01-02 17:03:37 +0100

    rehype external links

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,20 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import rehypeExternalLinks from 'rehype-external-links';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://david.deft.work',
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          content: { type: 'text', value: ' 🔗' }
+        }
+      ],
+    ]
+  },
 });
