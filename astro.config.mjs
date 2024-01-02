ac9b812cc2b5fa93d7b480f45b9a9c70f5ceaa85
Eloy López <elswork@gmail.com>
2024-01-02 19:17:59 +0100

    add icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,26 +3,24 @@ import tailwind from "@astrojs/tailwind";
 import rehypeExternalLinks from 'rehype-external-links';
 import partytown from "@astrojs/partytown";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    partytown({
-      // Adds dataLayer.push as a forwarding-event.
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-    tailwind(), // <-- Mover tailwind() dentro del array
-  ],
+  integrations: [partytown({
+    // Adds dataLayer.push as a forwarding-event.
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), tailwind() // <-- Mover tailwind() dentro del array
+  , icon()],
   site: 'https://david.deft.work',
   markdown: {
-    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        {
-          content: { type: 'text', value: ' 🔗' }
-        }
-      ],
-    ]
-  },
+    rehypePlugins: [[rehypeExternalLinks, {
+      content: {
+        type: 'text',
+        value: ' 🔗'
+      }
+    }]]
+  }
 });
