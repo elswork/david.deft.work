f2078c8b74be4ccca53cd7812be127173672188d
Eloy López <elswork@gmail.com>
2024-01-02 19:55:49 +0100

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import rehypeExternalLinks from 'rehype-external-links';
 import partytown from "@astrojs/partytown";
-
 import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,7 +14,7 @@ export default defineConfig({
       forward: ["dataLayer.push"]
     }
   }), tailwind() // <-- Mover tailwind() dentro del array
-  , icon()],
+  , icon(), sitemap()],
   site: 'https://david.deft.work',
   markdown: {
     rehypePlugins: [[rehypeExternalLinks, {
